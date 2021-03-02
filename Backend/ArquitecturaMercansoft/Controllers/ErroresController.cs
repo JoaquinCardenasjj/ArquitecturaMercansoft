15c36cba4cab42ea53a12ba65a563a384f06be88
Jose Joaquin Cardenas Perez <jcardenasp@itcol.com>
2021-03-02 09:46:27 -0500

    Ajuste CRUD clientes

---
+++ Backend/ArquitecturaMercansoft/Controllers/ErroresController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArquitecturaMercansoft.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErroresController : ControllerBase
+    {
+        [Route("error")]
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            return Problem(
+                detail: context.Error.StackTrace,
+                title: context.Error.Message);
+        }
+    }
+}
