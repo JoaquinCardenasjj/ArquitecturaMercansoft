35f08668978fc0a7e249b64eadd7cfef381ace84
Jose Joaquin Cardenas Perez <jjperez@latam.stefanini.com>
2021-03-04 19:31:45 -0500

    Ajustes

---
+++ Backend/ArquitecturaMercansoft/Transversal/Transversal/RegisterResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transversal.Transversal
+{
+    public class RegisterResponse
+    {
+        public bool Exitoso { get; set; }
+        public string CodigoResultado { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
