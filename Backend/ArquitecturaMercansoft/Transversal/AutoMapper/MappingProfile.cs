3deddf3d4567c5c2c2981d4cbc008fb7b1ffdaf1
Jose Joaquin Cardenas Perez <jjperez@latam.stefanini.com>
2021-03-04 19:24:18 -0500

    Cambios profile

---
+++ Backend/ArquitecturaMercansoft/Transversal/AutoMapper/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Transversal.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Transversal.Entities;
+
+namespace Transversal.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Cliente, ClienteDTO>();
+
+            CreateMap<ClienteDTO, Cliente>();
+
+            CreateMap<Cliente, Cliente>();
+            // Se pueden agregar cualquier numero de maps
+        }
+
+    }
+}
